53ed5dcf215d1f5cfce408c8d849b31e487e3cd4
EdwardMayk <samaelminina322@gmail.com>
2023-04-21 14:52:43 -0500

    products

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import Navegation from "../components/Navegation"
+import '../styles/globals.css'
 
 export const metadata = {
   title: 'Next.js',
--- components/Navegation.jsx
+++ components/Navegation.jsx
@@ -1,33 +1,38 @@
-import Link from 'next/link'
-import Style from '../components/Navegation.module.css'
+import Link from "next/link";
+import "tailwindcss/tailwind.css";
+import Style from "../components/Navegation.module.css";
 
-const links =[{
-    label: 'Home',
-    route:'/'
-},{
-    label: 'Products',
-    route: '/products'
-},{
-    label: 'Notification',
-    route: '/notification '
-}]
+const links = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Products",
+    route: "/products",
+  },
+  {
+    label: "Notification",
+    route: "/notification ",
+  },
+];
 
-export default function NavigationPage (){
-    return(
-        <header className={Style.header}>
-            <nav>
-                <ul className={Style.navegation}>
-                    <li>
-                        <Link href="/">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link href="/products">Productos</Link>
-                    </li>
-                    <li>
-                        <Link href="/notification">Notificaciones</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
-}   
+export default function NavigationPage() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex justify-center space-x-6 text-gray-600 text-sm font-medium">
+          <li className="hover:text-gray-900">
+            <Link href="/">Inicio</Link>
+          </li>
+          <li className="hover:text-gray-900">
+            <Link href="/products">Productos</Link>
+          </li>
+          <li className="hover:text-gray-900">
+            <Link href="/notification">Notificaciones</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
