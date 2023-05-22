a273698b65a66e26b11da97746818f84b1fdce6e
SchuwiProfessional <alvaxxx170375@gmail.com>
2023-05-22 16:31:37 -0500

    arregladamaestra

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+//import { v4 as uuidv4 } from "uuid";
 import "styles/globals.css";
 import "./custom.css";
 
