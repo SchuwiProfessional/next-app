b90274618861d0b01ef823d314cb9bf5ecdc5a2e
EdwardMayk <samaelminina322@gmail.com>
2023-05-22 23:00:52 -0500

    IMAGES

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true
   },
   reactStrictMode: true,
+  images: {
+    domains: ['64.media.tumblr.com', 'encrypted-tbn0.gstatic.com'] // Agrega aquí los dominios de las imágenes que deseas usar
+  }
 }
 
 module.exports = nextConfig
